f72c5fce3cd25e7a40f2537eb837a8df32cf5a03
JaneSoo <chimkanitha@gmail.com>
2017-06-20 05:15:03 +0700

    add firebase

--- app/src/main/java/com/example/janesoo/reankour/AllBottomNavigationViewActivity.java
+++ app/src/main/java/com/example/janesoo/reankour/AllBottomNavigationViewActivity.java
@@ -2,6 +2,7 @@ package com.example.janesoo.reankour;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -9,6 +10,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.janesoo.reankour.fragment.BottomNavigationFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * Created by JaneSoo on 17-Jun-17.
@@ -16,14 +19,34 @@ import com.example.janesoo.reankour.fragment.BottomNavigationFragment;
 
 public class AllBottomNavigationViewActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_layout);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    Intent intent = new Intent(AllBottomNavigationViewActivity.this,LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            }
+        };
+
         getSupportFragmentManager().beginTransaction().add(R.id.main,new BottomNavigationFragment()).commit();
-//        viewPager = (ViewPager) findViewById(R.id.viewpager);
-//        adapter = new AppPagerAdapter(getSupportFragmentManager());
-//        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
@@ -44,10 +67,11 @@ public class AllBottomNavigationViewActivity extends AppCompatActivity {
         }
 
         if(item.getItemId() == R.id.logout){
-            Intent intent = new Intent(AllBottomNavigationViewActivity.this, LoginActivity.class);
-            startActivity(intent);
+            logout();
         }
-
         return super.onOptionsItemSelected(item);
+    }
+    private void logout(){
+        mAuth.signOut();
     }
 }
--- app/src/main/java/com/example/janesoo/reankour/LoginActivity.java
+++ app/src/main/java/com/example/janesoo/reankour/LoginActivity.java
@@ -2,11 +2,19 @@ package com.example.janesoo.reankour;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by JaneSoo on 14-Jun-17.
@@ -16,20 +24,41 @@ public class LoginActivity extends AppCompatActivity {
 
     Button login;
     TextView signup;
+    EditText email, password;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
+        mAuth = FirebaseAuth.getInstance();
+
         login = (Button) findViewById(R.id.loginbtn);
         signup = (TextView) findViewById(R.id.textSignup);
+        email = (EditText) findViewById(R.id.loginemail);
+        password = (EditText) findViewById(R.id.loginpass);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, AllBottomNavigationViewActivity.class);
-                startActivity(intent);
+                boolean test = true;
+
+                if(email.getText().toString().equals("")){
+                    email.setError("Required!");
+                    test = false;
+                }
+                if(password.getText().toString().equals("")){
+                    password.setError("Required");
+                    test = false;
+                }
+
+                if(test==true){
+                    checkLogin();
+                }
+
             }
         });
         signup.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +68,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void checkLogin(){
+        String memail = email.getText().toString().trim();
+        String mpassword = password.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(memail,mpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, AllBottomNavigationViewActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Error login!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
