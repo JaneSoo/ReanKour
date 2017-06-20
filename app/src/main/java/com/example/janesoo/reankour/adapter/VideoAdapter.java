b67681388b3957dd5507c9d3688db87968bd53aa
SoSorphorn <phorndeveloper@gmail.com>
2017-06-20 03:29:26 -0500

    Update content

--- app/src/main/java/com/example/janesoo/reankour/adapter/VideoAdapter.java
+++ app/src/main/java/com/example/janesoo/reankour/adapter/VideoAdapter.java
@@ -1,27 +1,35 @@
 package com.example.janesoo.reankour.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import com.example.janesoo.reankour.model.Video;
+
+import java.util.ArrayList;
 
 /**
  * Created by JaneSoo on 17-Jun-17.
  */
 
 public class VideoAdapter extends BaseAdapter {
+    private ArrayList<Video> arrayList;
+    private int layout;
+    private Context context;
     @Override
     public int getCount() {
-        return 0;
+        return arrayList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return arrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
