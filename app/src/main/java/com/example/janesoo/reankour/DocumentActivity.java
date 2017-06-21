02e2b42a969952b311a00de576ba872677b193cd
SoSorphorn <phorndeveloper@gmail.com>
2017-06-21 15:22:24 -0500

    fix conflict

--- app/src/main/java/com/example/janesoo/reankour/DocumentActivity.java
+++ app/src/main/java/com/example/janesoo/reankour/DocumentActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
@@ -45,14 +46,16 @@ public class DocumentActivity extends AppCompatActivity {
 
         buttonReadMore=(Button)findViewById(R.id.btn_readMore);
 
-       /* buttonReadMore.setOnClickListener(new View.OnClickListener() {
+        /*buttonReadMore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DocumentActivity.this,DetailDocumentActivity.class);
                 startActivity(intent);
 
             }
-        });*/
+        });
+*/
+
 
     }
 }
