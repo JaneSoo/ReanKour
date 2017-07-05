f6dcafafd0fcca0286e87e36644367058d3d080f
koemsreangkor <lisreang03@gmail.com>
2017-07-05 09:48:46 +0700

    remove

--- app/src/main/java/com/example/janesoo/reankour/fragment/FragmentVideo.java
+++ app/src/main/java/com/example/janesoo/reankour/fragment/FragmentVideo.java
@@ -38,11 +38,11 @@ public class FragmentVideo extends Fragment {
         ArrayList<Document> newDocument =new ArrayList<>();
 
 
-        newDocument.add(new Document(R.drawable.university,"The way to find university","There are so many to choose the university in cambodia","12 June 17"));
+        newDocument.add(new Document(R.drawable.universityicon,"The way to find universityicon","There are so many to choose the universityicon in cambodia","12 June 17"));
         newDocument.add(new Document(R.drawable.speaking,"How to speak English","There are so many way that can help you to speak English very well","19 June 17"));
         newDocument.add(new Document(R.drawable.scholarship,"Scholarship","Scholarship to openning now such as Singapore, Korea, Indonesi","10 June 17"));
         newDocument.add(new Document(R.drawable.develop,"How to develop yourself","There are so many that can develop yourself to be stronger ","12 June 17"));
-        newDocument.add(new Document(R.drawable.content1,"The way to find university","There are so many to choose the university in cambodia","12 June 17"));
+        newDocument.add(new Document(R.drawable.content1,"The way to find universityicon","There are so many to choose the universityicon in cambodia","12 June 17"));
         newDocument.add(new Document(R.drawable.content2,"How to speak English","There are so many way that can help you to speak English very well","19 June 17"));
         newDocument.add(new Document(R.drawable.content3,"Scholarship","Scholarship to openning now such as Singapore, Korea, Indonesi","10 June 17"));
         newDocument.add(new Document(R.drawable.content4,"How to develop yourself","There are so many that can develop yourself to be stronger ","12 June 17"));
