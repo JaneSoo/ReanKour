bf3284599e2d45da3531656807cf0d3510c42168
SoSorphorn <phorndeveloper@gmail.com>
2017-07-11 04:59:35 -0500

    create video in subject by category

--- app/src/main/java/com/example/janesoo/reankour/VideoActivity.java
+++ app/src/main/java/com/example/janesoo/reankour/VideoActivity.java
@@ -10,6 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import com.example.janesoo.reankour.adapter.VideoAdapter;
 import com.example.janesoo.reankour.model.SubjectModel;
 import com.example.janesoo.reankour.model.Video;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,24 +32,62 @@ public class VideoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_layout);
 
-      /*  Intent intent = getIntent();
-        SubjectModel subjectModel = (SubjectModel) intent.getSerializableExtra("Subject");*/
+       Intent intent = getIntent();
+        final SubjectModel subjectModel = (SubjectModel) intent.getSerializableExtra("Subject");
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview_home);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
-        //loading data
+        /*//loading data
         list = new ArrayList<>();
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/HO8UvN2DV4A\" frameborder=\"0\" allowfullscreen></iframe>","How to good at English speaking","12July17"));
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/21ioywVyjrY\" frameborder=\"0\" allowfullscreen></iframe>","How to good at Math","10July17"));
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/M8xCj2VPHas\" frameborder=\"0\" allowfullscreen></iframe>","How to solve physic problem","10May17"));
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/3zd-rCr8RgE\" frameborder=\"0\" allowfullscreen></iframe>","Chemistry Physics","1April17"));
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/CX84l5ZZHVg\" frameborder=\"0\" allowfullscreen></iframe>","Batteries and How They Work ","9March17"));
-        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/cuCSELaQ_Go\" frameborder=\"0\" allowfullscreen></iframe>","Aseptic techniques in the biology lab","10January17"));
+        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/HO8UvN2DV4A\" frameborder=\"0\" allowfullscreen></iframe>","How to good at English speaking","12July17","english"));
+        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/21ioywVyjrY\" frameborder=\"0\" allowfullscreen></iframe>","How to good at Math","10July17","math"));
+        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/M8xCj2VPHas\" frameborder=\"0\" allowfullscreen></iframe>","How to solve physic problem","10May17","physic"));
+        list.add(new Video("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/3zd-rCr8RgE\" frameborder=\"0\" allowfullscreen></iframe>","Chemistry Physics","1April17","chemistry"));
 
-        VideoAdapter adapter = new VideoAdapter(list);
-        recyclerView.setAdapter(adapter);
+        //test category
+        ArrayList<Video> subVideo = new ArrayList<>();
+
+        for(int i = 0; i < list.size();i++)
+        {
+            if(list.get(i).getCategory().equals(subjectModel.getCategory()))
+            {
+                subVideo.add(list.get(i));
+            }
+        }*/
+
+
+
+        //get video from firebase:
+
+        DatabaseReference mDataRef = FirebaseDatabase.getInstance().getReference("Videos");
+        final  ArrayList<Video> newVideo =new ArrayList<>();
+
+        mDataRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                newVideo.clear();
+                for(DataSnapshot videoSnapshot : dataSnapshot.getChildren()){
+                    Video video = videoSnapshot.getValue(Video.class);
+                    if(video.getCategory().equals(subjectModel.getCategory()))
+                    {
+                        newVideo.add(video);
+
+                    }
+                    VideoAdapter adapter = new VideoAdapter(newVideo);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
